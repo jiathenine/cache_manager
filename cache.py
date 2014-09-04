79080514cd428d2d5e6c782dee4e36abbea0bd9c
U-Leo-PC\Leo <Leo@jia-PC.(none)>
2014-09-04 18:02:52 +0800

    oh ,some file edit

--- cache.py
+++ cache.py
@@ -1,8 +1,20 @@
 __author__ = 'jia'
-
+import beaker.cache
+import inspect
+import types
 region_config = {}
 
-def cache_region(region, manager, *region_args):
+
+class CacheManager(object):
+    pass
+
+
+def is_mem_func(func):
+    return hasattr(func, '__self__') or inspect.getargspec(func).get()
+
+
+def cache_region(region, manager, skip_pos=0, *deco_args):
     def decorator(func):
-        def _decorator(*args, **kwargs):
-            
+        def _decorator(*args):
+            cache_ = region_config[manager].get_dbmanager(region)
+            key = ' '.join(map(str, deco_args + region[skip_pos:]))
